de258e63fc4265ff85437d70825068e0570fc2b0
Trần Lê Duy <tranleduy1233@gmail.com>
2017-07-04 20:33:49 +0700

    change color

--- app/src/main/java/com/duy/acsiigenerator/MainActivity.java
+++ app/src/main/java/com/duy/acsiigenerator/MainActivity.java
@@ -1,44 +1,27 @@
-/*
- * Copyright (c) 2017 by Tran Le Duy
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.duy.acsiigenerator;
 
 import android.content.ActivityNotFoundException;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewTreeObserver;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
-import com.duy.acsiigenerator.figlet.TextConvertPresenter;
+import com.duy.acsiigenerator.emoticons.fragment.EmoticonFragment;
+import com.duy.acsiigenerator.emoticons.fragment.TextImageFragment;
+import com.duy.acsiigenerator.figlet.TextFragment;
+import com.duy.acsiigenerator.image.ImageToAsciiFragment;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import imagetotext.duy.com.asciigenerator.BuildConfig;
 import imagetotext.duy.com.asciigenerator.R;
@@ -46,13 +29,11 @@ import imagetotext.duy.com.asciigenerator.R;
 public class MainActivity extends AppCompatActivity {
 
     public static final int EXTERNAL_READ_PERMISSION_GRANT = 1212;
-    private static final String TAG = "MainActivity";
-    CoordinatorLayout coordinatorLayout;
-    Toolbar toolbar;
-    private KeyBoardEventListener keyBoardListener;
-    private ViewPager viewPager;
-    private PagerSectionAdapter adapter;
-    private TextConvertPresenter mTextPresenter;
+    private AdView mAdView;
+    private Fragment mTextFragment;
+    private Fragment mImageToAsciiFragment;
+    private Fragment mEmoticonFragment;
+    private Fragment mTextImageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,105 +41,103 @@ public class MainActivity extends AppCompatActivity {
         hideStatusBar();
         setContentView(R.layout.activity_main);
 
-        this.coordinatorLayout = (CoordinatorLayout) findViewById(R.id.container);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mTextFragment = TextFragment.newInstance();
+        mImageToAsciiFragment = ImageToAsciiFragment.newInstance();
+        mEmoticonFragment = new EmoticonFragment();
+        mTextImageFragment = new TextImageFragment();
 
-        Intent intent = getIntent();
-        String action = intent.getAction();
-        String type = intent.getType();
-        String text = null;
-        if (Intent.ACTION_SEND.equals(action) && type != null) {
-            if (type.equals("text/plain")) {
-                text = intent.getStringExtra(Intent.EXTRA_TEXT);
-            }
-        }
-
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        adapter = new PagerSectionAdapter(getSupportFragmentManager(), text);
-        viewPager.setOffscreenPageLimit(adapter.getCount());
-        viewPager.setAdapter(adapter);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                if (position == AdsFragment.INDEX) {
-                    hideKeyboard();
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
-        tabLayout.setupWithViewPager(viewPager);
-
-        //attach listener hide/show keyboard
-        keyBoardListener = new KeyBoardEventListener(this);
-        coordinatorLayout.getViewTreeObserver().addOnGlobalLayoutListener(keyBoardListener);
-    }
-
-    private void hideKeyboard() {
-        View view = this.getCurrentFocus();
-        if (view != null) {
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_more:
-                moreApp();
-                break;
-            case R.id.action_rate:
-                goToPlayStore();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * hide appbar layout when keyboard visible
-     */
-    private void hideAppBar() {
-        toolbar.setVisibility(View.GONE);
-    }
-
-    /**
-     * show appbar layout when keyboard gone
-     */
-    private void showAppBar() {
-        toolbar.setVisibility(View.VISIBLE);
-    }
-
-    protected void onShowKeyboard() {
-        hideAppBar();
-    }
-
-    protected void onHideKeyboard() {
-        showAppBar();
+        bindView();
+        showAdView();
     }
 
     private void hideStatusBar() {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
+
+    private void showAdView() {
+        mAdView = (AdView) findViewById(R.id.ad_view);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AdRequest adRequest = new AdRequest.Builder().build();
+                mAdView.loadAd(adRequest);
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAdView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAdView.pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mAdView.destroy();
+    }
+
+    private void bindView() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_text: {
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content, mTextFragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                    case R.id.action_image: {
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content, mImageToAsciiFragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                    case R.id.action_emoticon: {
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content, mEmoticonFragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                    case R.id.action_text_image: {
+                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.content, mTextImageFragment);
+                        fragmentTransaction.commit();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_text);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_more_app:
+                        moreApp();
+                        return true;
+                    case R.id.action_rate:
+                        goToPlayStore();
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
 
     private void moreApp() {
         Uri uri = Uri.parse("market://search?q=pub:Trần Lê Duy");
@@ -191,6 +170,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     //Add this method to show Dialog when the required permission has been granted to the app.
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
@@ -201,31 +181,6 @@ public class MainActivity extends AppCompatActivity {
                     //Permission has not been granted. Notify the user.
                     Toast.makeText(MainActivity.this, R.string.permission_msg, Toast.LENGTH_SHORT).show();
                 }
-            }
-        }
-    }
-
-    private class KeyBoardEventListener implements ViewTreeObserver.OnGlobalLayoutListener {
-        MainActivity activity;
-
-        KeyBoardEventListener(MainActivity activityIde) {
-            this.activity = activityIde;
-        }
-
-        public void onGlobalLayout() {
-            int i = 0;
-            int navHeight = this.activity.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
-            navHeight = navHeight > 0 ? this.activity.getResources().getDimensionPixelSize(navHeight) : 0;
-            int statusBarHeight = this.activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
-            if (statusBarHeight > 0) {
-                i = this.activity.getResources().getDimensionPixelSize(statusBarHeight);
-            }
-            Rect rect = new Rect();
-            activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
-            if (activity.coordinatorLayout.getRootView().getHeight() - ((navHeight + i) + rect.height()) <= 0) {
-                activity.onHideKeyboard();
-            } else {
-                activity.onShowKeyboard();
             }
         }
     }
