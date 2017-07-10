89951b6d0f07681e5cac4488a19ac4bfc4fdbfae
Trần Lê Duy <tranleduy1233@gmail.com>
2017-07-10 15:16:35 +0700

    clean code

--- apppro/src/main/java/com/duy/asciigenerator/pro/MainActivity.java
+++ apppro/src/main/java/com/duy/asciigenerator/pro/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_more_app:
-                        moreApp();
+                        StoreUtil.moreApp(MainActivity.this);
                         return true;
                     case R.id.action_rate:
                         StoreUtil.gotoPlayStore(MainActivity.this, BuildConfig.APPLICATION_ID);
@@ -95,23 +95,6 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
             }
         });
     }
-
-
-    private void moreApp() {
-        Uri uri = Uri.parse("market://search?q=pub:Trần Lê Duy");
-        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-        // To count with Play market backstack, After pressing back button,
-        // to taken back to our application, we need to add following flags to intent.
-        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-        try {
-            startActivity(goToMarket);
-        } catch (ActivityNotFoundException e) {
-            startActivity(new Intent(Intent.ACTION_VIEW,
-                    Uri.parse("http://play.google.com/store/search?q=pub:Trần Lê Duy")));
-        }
-
-    }
-
 
 
     @Override
