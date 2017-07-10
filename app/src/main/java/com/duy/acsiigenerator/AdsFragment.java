b086748f7b03efd2298838dd814bfa8bd83bfde4
Trần Lê Duy <tranleduy1233@gmail.com>
2017-07-10 13:06:18 +0700

    add button get pro

--- app/src/main/java/com/duy/acsiigenerator/AdsFragment.java
+++ app/src/main/java/com/duy/acsiigenerator/AdsFragment.java
@@ -23,6 +23,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.duy.ascii.sharedcode.StoreUtil;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.NativeExpressAdView;
 
@@ -55,10 +56,16 @@ public class AdsFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.btn_pro_version).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StoreUtil.gotoPlayStore(getActivity(), "com.duy.text_converter.pro");
+            }
+        });
+
         mAdView = view.findViewById(R.id.ad_view);
-
         AdRequest adRequest = new AdRequest.Builder().build();
-
         // Start loading the ad in the background.
         mAdView.loadAd(adRequest);
     }
