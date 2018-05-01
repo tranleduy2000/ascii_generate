1a9880f0182ee1a4efb84b73f9dfc51756a51c04
Duy <tranleduy1233@gmail.com>
2018-05-01 21:21:06 +0700

    Remove ignored files

--- app/copy_release.bat
+++
@@ -1,4 +0,0 @@
-mkdir release
-cp build\outputs\apk\free\release\app-free-release.apk release/app-free-release.apk
-cp build\outputs\apk\pro\release\app-pro-release.apk release/app-pro-release.apk
-exit
--- app/install-pro.bat
+++
@@ -1,4 +0,0 @@
-adb uninstall com.duy.asciigenerator.pro
-adb install -r release\app-pro-release.apk
-adb shell am start -n " com.duy.asciigenerator.pro/com.duy.ascii.art.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
-exit
